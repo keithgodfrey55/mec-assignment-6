225c5e291b63c242889376dfc179484d66de7370
Michael Casey <caseymichaelt1893@gmail.com>
2021-01-12 14:51:13 -0500

    Added compinent

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,54 +1,86 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
 
-
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  render() {
     return (
       <div>
         <Grid container spacing={2}>
-          <Grid item xs={12} sm={12} >
-            <h1>Inteliband</h1>
-          </Grid>
+          <Grid item xs={12} sm={12}>
+            <h1> Inteliband where our first priority is head </h1>{" "}
+          </Grid>{" "}
           <Grid item xs={12} sm={6}>
-            <Paper elevation ={2}>
-              <h1>about us</h1>
-              <p>We are a retail tech company focused in the smartwatch field. We created 
-                Inteliband to help people get assistance as soon as irregular patterns are detected in a persons vitals.
-              </p>
-              </Paper>
-          </Grid>
+            <Paper elevation={2}>
+              <h1> about us </h1>{" "}
+              <p>
+                {" "}
+                We are a retail tech company focused in the smartwatch field.We
+                created Inteliband to help people get assistance as soon as
+                irregular patterns are detected in a persons vitals.{" "}
+              </p>{" "}
+            </Paper>{" "}
+          </Grid>{" "}
           <Grid item xs={12} sm={6}>
-            <Paper elevation ={2}>
-              <h1>Mission Statement</h1>
-              <p> Deliver the fastest possible assistance to people in need.
-              </p>
-              </Paper>
-          </Grid>
+            <Paper elevation={2}>
+              <h1> Mission Statement </h1>{" "}
+              <p>
+                {" "}
+                Deliver the fastest possible assistance to people in need.{" "}
+              </p>{" "}
+            </Paper>{" "}
+          </Grid>{" "}
           <Grid item xs={12} sm={6}>
-            <Paper elevation ={2}>
-            <Accordion>
-              <AccordionSummary>
-                 <Typography><h1>How Inteliband works</h1> <p>*click here*</p></Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                <p>The Inteliband works by placing a heart rate sensor on the undersode of the watch and monitors for 
-                  irregular vital patterns and alerts the paramedics/authorities once detected.
-                </p>
-                </Typography>
-                </AccordionDetails>
-            </Accordion>
-              </Paper>
-          </Grid>
-        </Grid>
+            <Paper elevation={2}>
+              <Accordion>
+                <AccordionSummary>
+                  <Typography>
+                    {" "}
+                    <h1> How Inteliband works </h1> <p>*click here*</p>{" "}
+                  </Typography>{" "}
+                </AccordionSummary>{" "}
+                <AccordionDetails>
+                  <Typography>
+                    <p>
+                      {" "}
+                      The Inteliband works by placing a heart rate sensor on the
+                      undersode of the watch and monitors for irregular vital
+                      patterns and alerts the paramedics / authorities once
+                      detected.{" "}
+                    </p>{" "}
+                  </Typography>{" "}
+                </AccordionDetails>{" "}
+              </Accordion>{" "}
+            </Paper>{" "}
+          </Grid>{" "}
+          <Grid item xs={12} sm={6}>
+            <Paper elevation={2}>
+              <Accordian>
+                <AccordionSummary>
+                  <Typography>
+                    {" "}
+                    <h1> Versions </h1> <p>*click here*</p>{" "}
+                  </Typography>{" "}
+                </AccordionSummary>{" "}
+                <AccordionDetails>
+                  <Typography>
+                    <ul>
+                      <li> Intelliband 0.5 15 GB </li>{" "}
+                      <li> Intelliband 1.5 32 GB </li>{" "}
+                      <li> Intelliband 2.0 64 GB </li>{" "}
+                    </ul>{" "}
+                  </Typography>{" "}
+                </AccordionDetails>{" "}
+              </Accordian>{" "}
+            </Paper>{" "}
+          </Grid>{" "}
+        </Grid>{" "}
       </div>
-    ); 
+    );
   }
 }
 export default App;
