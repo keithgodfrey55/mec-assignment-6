376e02ebcda49705c8e3bc1e14601104b4de01ec
Keith Godfrey <keithgodfrey@Keiths-MacBook-Pro.local>
2021-01-12 15:28:31 -0500

    heading

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,75 +10,52 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={12}>
-            <h1> Inteliband where our first priority is head </h1>{" "}
-          </Grid>{" "}
-          <Grid item xs={12} sm={6}>
+        <Grid container direction="row" jutstify="space-evenly" alignItems="center" spacing={2}>
+          <Grid item align="center" xs={12} sm={12}>
+            <Typography variant="h2">Inteliband</Typography> <br />
+            <Typography>"where our first priority is you"</Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
             <Paper elevation={2}>
-              <h1> about us </h1>{" "}
+            <Typography>
+              <h1>About us</h1>
               <p>
-                {" "}
-                We are a retail tech company focused in the smartwatch field.We
+                We are a retail tech company focused in the smartwatch field. We
                 created Inteliband to help people get assistance as soon as
-                irregular patterns are detected in a persons vitals.{" "}
-              </p>{" "}
-            </Paper>{" "}
-          </Grid>{" "}
-          <Grid item xs={12} sm={6}>
-            <Paper elevation={2}>
-              <h1> Mission Statement </h1>{" "}
-              <p>
-                {" "}
-                Deliver the fastest possible assistance to people in need.{" "}
-              </p>{" "}
-            </Paper>{" "}
-          </Grid>{" "}
-          <Grid item xs={12} sm={6}>
+                irregular patterns are detected in a persons vitals.
+              </p>
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper elevation={2}><Typography>
+              <h1>Mission Statement</h1>
+              <p> Deliver the fastest possible assistance to people in need.</p>
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
             <Paper elevation={2}>
               <Accordion>
                 <AccordionSummary>
                   <Typography>
-                    {" "}
-                    <h1> How Inteliband works </h1> <p>*click here*</p>{" "}
-                  </Typography>{" "}
-                </AccordionSummary>{" "}
+                    <h1>How Inteliband works</h1> <p>*click here*</p>
+                  </Typography>
+                </AccordionSummary>
                 <AccordionDetails>
                   <Typography>
                     <p>
-                      {" "}
                       The Inteliband works by placing a heart rate sensor on the
                       undersode of the watch and monitors for irregular vital
-                      patterns and alerts the paramedics / authorities once
-                      detected.{" "}
-                    </p>{" "}
-                  </Typography>{" "}
-                </AccordionDetails>{" "}
-              </Accordion>{" "}
-            </Paper>{" "}
-          </Grid>{" "}
-          <Grid item xs={12} sm={6}>
-            <Paper elevation={2}>
-              <Accordion>
-                <AccordionSummary>
-                  <Typography>
-                    {" "}
-                    <h1> Versions </h1> <p>*click here*</p>{" "}
-                  </Typography>{" "}
-                </AccordionSummary>{" "}
-                <AccordionDetails>
-                  <Typography>
-                    <ul>
-                      <li> Intelliband 0.5 15 GB </li>{" "}
-                      <li> Intelliband 1.5 32 GB </li>{" "}
-                      <li> Intelliband 2.0 64 GB </li>{" "}
-                    </ul>{" "}
-                  </Typography>{" "}
-                </AccordionDetails>{" "}
-              </Accordion>{" "}
-            </Paper>{" "}
-          </Grid>{" "}
-        </Grid>{" "}
+                      patterns and alerts the paramedics/authorities once
+                      detected.
+                    </p>
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     );
   }
