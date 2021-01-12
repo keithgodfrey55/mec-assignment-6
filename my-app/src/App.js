20e759902fc8a210dbe4d402d59faee700062813
Keith Godfrey <keithgodfrey@Keiths-MacBook-Pro.local>
2021-01-12 13:20:30 -0500

    material update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-
+import Paper from '@material-ui/core/Paper';
 
 class App extends React.Component{
   render(){
     return (
       <div>
         <Grid container>
-          <h1>hello</h1>
+          <Grid item xs={12} sm={12} >
+            <h1>Inteliband</h1>
+          </Grid>
+          <Grid item>
+            <Paper elevation ={2}>about us</Paper>
+          </Grid>
         </Grid>
       </div>
     ); 
