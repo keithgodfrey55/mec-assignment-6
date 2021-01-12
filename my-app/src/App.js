4043461fac9c75ec7579277e5e3d1ca2356faac1
Keith Godfrey <keithgodfrey@Keiths-MacBook-Pro.local>
2021-01-12 16:10:45 -0500

    on the same page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,27 +10,36 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Grid container direction="row" jutstify="space-evenly" alignItems="center" spacing={2}>
+        <Grid
+          container
+          direction="row"
+          jutstify="space-evenly"
+          alignItems="center"
+          spacing={2}
+        >
           <Grid item align="center" xs={12} sm={12}>
             <Typography variant="h2">Inteliband</Typography> <br />
             <Typography>"where our first priority is you"</Typography>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
             <Paper elevation={2}>
-            <Typography>
-              <h1>About us</h1>
-              <p>
-                We are a retail tech company focused in the smartwatch field. We
-                created Inteliband to help people get assistance as soon as
-                irregular patterns are detected in a persons vitals.
-              </p>
+              <Typography>
+                <h1>About us</h1>
+                <p>
+                  We are a retail tech company focused in the smartwatch field.
+                  We created Inteliband to help people get assistance as soon as
+                  irregular patterns are detected in a persons vitals.
+                </p>
               </Typography>
             </Paper>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <Paper elevation={2}><Typography>
-              <h1>Mission Statement</h1>
-              <p> Deliver the fastest possible assistance to people in need.</p>
+            <Paper elevation={2}>
+              <Typography>
+                <h1>Mission Statement</h1>
+                <p>
+                  Deliver the fastest possible assistance to people in need.
+                </p>
               </Typography>
             </Paper>
           </Grid>
@@ -55,28 +64,26 @@ class App extends React.Component {
               </Accordion>
             </Paper>
           </Grid>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={6} md={4}>
             <Paper elevation={2}>
               <Accordion>
                 <AccordionSummary>
                   <Typography>
-                    {" "}
-                    <h1> Versions </h1> <p>*click here*</p>{" "}
-                  </Typography>{" "}
-                </AccordionSummary>{" "}
+                    <h1> Versions </h1> <p>*click here*</p>
+                  </Typography>
+                </AccordionSummary>
                 <AccordionDetails>
                   <Typography>
                     <ul>
-                      <li> Intelliband 0.5 15 GB </li>{" "}
-                      <li> Intelliband 1.5 32 GB </li>{" "}
-                      <li> Intelliband 2.0 64 GB </li>{" "}
-                    </ul>{" "}
-                  </Typography>{" "}
-                </AccordionDetails>{" "}
-              </Accordion>{" "}
-            </Paper>{" "}
-          </Grid>{" "}
-
+                      <li> Intelliband 0.5 15 GB </li>
+                      <li> Intelliband 1.5 32 GB </li>
+                      <li> Intelliband 2.0 64 GB </li>
+                    </ul>
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </Paper>
+          </Grid>
         </Grid>
       </div>
     );
