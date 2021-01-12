e28fea31926d58094512d9afaba804f181123311
Keith Godfrey <keithgodfrey@Keiths-MacBook-Pro.local>
2021-01-12 14:10:06 -0500

    How Inteliband Works

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+
 
 class App extends React.Component{
   render(){
@@ -23,6 +28,22 @@ class App extends React.Component{
               <h1>Mission Statement</h1>
               <p> Deliver the fastest possible assistance to people in need.
               </p>
+              </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper elevation ={2}>
+            <Accordion>
+              <AccordionSummary>
+                 <Typography><h1>How Inteliband works</h1> <p>*click here*</p></Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                <p>The Inteliband works by placing a heart rate sensor on the undersode of the watch and monitors for 
+                  irregular vital patterns and alerts the paramedics/authorities once detected.
+                </p>
+                </Typography>
+                </AccordionDetails>
+            </Accordion>
               </Paper>
           </Grid>
         </Grid>
