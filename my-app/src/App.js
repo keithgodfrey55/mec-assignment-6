7ae59a137344a0cfc4c3a136c0769d4ec0f900e0
Keith Godfrey <keithgodfrey@Keiths-MacBook-Pro.local>
2021-01-12 13:50:58 -0500

    mission statement redo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,12 +6,24 @@ class App extends React.Component{
   render(){
     return (
       <div>
-        <Grid container>
+        <Grid container spacing={2}>
           <Grid item xs={12} sm={12} >
             <h1>Inteliband</h1>
           </Grid>
-          <Grid item>
-            <Paper elevation ={2}>about us</Paper>
+          <Grid item xs={12} sm={6}>
+            <Paper elevation ={2}>
+              <h1>about us</h1>
+              <p>We are a retail tech company focused in the smartwatch field. We created 
+                Inteliband to help people get assistance as soon as irregular patterns are detected in a persons vitals.
+              </p>
+              </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper elevation ={2}>
+              <h1>Mission Statement</h1>
+              <p> Deliver the fastest possible assistance to people in need.
+              </p>
+              </Paper>
           </Grid>
         </Grid>
       </div>
