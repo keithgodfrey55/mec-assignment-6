b9a1da8f19ecbc45e1db3cca15e43b6d2b4b8230
Lulu <luismora.11131@gmail.com>
2021-01-12 20:21:01 -0500

    added some new info

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -110,6 +110,124 @@ class App extends React.Component {
                 Compatible Devices
             </Button>
           </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper elevation={2}>
+              <Typography>
+                <h1>
+                  North America Corporate Contacts
+                </h1>
+                <h3>
+                  United States
+                </h3>
+                <li>
+                  Inteliband Media Helpline (408) 974-2042
+                </li>
+                <li>
+                  Inteliband Software Upgrade Center (800) 840-8436
+                </li>
+                <li>
+                  Reseller Referral (Reseller, Trainers, Consultants)
+                  (800) 564-9412
+                </li>
+                <h3>
+                  Canada
+                </h3>
+                <li>
+                  Inteliband Store (Consumer and Education Individuals)
+                  (800) 857-3438
+                </li>
+                <h3>
+                  Mexico
+                </h3>
+                <li>
+                  Inteliband Store (Consumer and Education Individuals)
+                  (001-800-857-3438)
+                </li>
+                <li>
+                  Inteliband Store (Small Business) 001-800-857-3438
+                </li>
+                
+                
+                
+              </Typography>
+            </Paper>
+          </Grid>
+           <Grid item xs={12} sm={6} md={4}>
+            <Paper elevation={2}>
+              <Typography>
+                <h1>
+                  Frequently Requested Info
+                </h1>
+                <h3>
+                  Inteliband Login Support
+                </h3>
+                <p>
+                  Learn more about logins and its benefits.
+                </p>
+                <a href="http://localhost:3000/">Learn more ></a>
+                <h3>
+                  Inteliband Products
+                </h3>
+                <p>
+                Find out how to get additional technical support and hardware service options for your Inteliband products.
+                </p>
+                <a href="http://localhost:3000/">Learn more ></a>
+                <h3>
+                  Repair and Services
+                </h3>
+                <p>
+                  See all your repair and services options based on your product and location.
+                </p>
+                <a href="http://localhost:3000/">Learn more ></a>
+                <h3>
+                  Repair Status
+                </h3>
+                <p>
+                Quickly and easily get the status of one or all of your repairs.
+                </p>
+                <a href="http://localhost:3000/">Learn more ></a>
+                <h3>
+                  Job Opportunities
+                </h3>
+                <p>
+                Find current openings, college jobs, internships, and more.
+                </p>
+                <a href="http://localhost:3000/">Learn more ></a>
+                <h3>
+                Media and Analyst Info
+                </h3>
+                <p>
+                Get press releases, media contacts, and more.
+                </p>
+                <a href="http://localhost:3000/">Learn more ></a>
+                <h3>
+                Email Subscriptions
+                </h3>
+                <p>
+                Update your email address or change your subscription status.
+                </p>
+                <a href="http://localhost:3000/">Learn more ></a>
+                <h3>
+                User Groups
+                </h3>
+                <p>
+                Mix and mingle with other Inteliband Users in your area.
+                </p>
+                <a href="http://localhost:3000/">Learn more ></a>
+              </Typography>
+              </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper elevation={2}>
+              <Typography>
+              <h1>
+              Feedback
+              </h1>
+              <p>Tell us how we’re doing. Select the appropriate feedback option (we read everything, but can’t always respond):</p>
+              </Typography>
+            </Paper>
+          </Grid>
         </Grid>
       </div>
     );
